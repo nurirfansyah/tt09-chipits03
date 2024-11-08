fe35769318b75ab508af43fcab112fd775096d6a
nurirfansyah <40015347+nurirfansyah@users.noreply.github.com>
2024-11-08 15:38:04 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_ams_its1 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
